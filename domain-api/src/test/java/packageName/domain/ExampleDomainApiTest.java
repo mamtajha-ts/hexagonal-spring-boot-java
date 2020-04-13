0f7ebd03e551e525e11d31790993f7466fec767f
paul58914080 <paul58914080@gmail.com>
2020-04-13 20:08:55 +0530

    test: added sample tests

---
+++ domain-api/src/test/java/packageName/domain/ExampleDomainApiTest.java
@@ -0,0 +1,18 @@
+package packageName.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class ExampleDomainApiTest {
+
+  @Test
+  @DisplayName("should init")
+  public void init() {
+    assertThat(Boolean.TRUE).isTrue();
+  }
+}
