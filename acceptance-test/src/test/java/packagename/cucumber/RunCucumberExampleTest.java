b15e6a05b034e8039c95cbe04504897bcba18b79
Paul <paul58914080@gmail.com>
2020-08-02 19:53:18 +0530

    build: changed from maven to gradle

--- acceptance-test/src/test/java/packagename/cucumber/RunCucumberExampleTest.java
+++ acceptance-test/src/test/java/packagename/cucumber/RunCucumberExampleTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:features", strict = true, plugin = {
+@CucumberOptions(features = "classpath:features", plugin = {
     "json:target/cucumber/example.json", "json:target/cucumber/example.xml"}, tags =
     "@Example", glue = "classpath:packagename.cucumber")
 public class RunCucumberExampleTest {
